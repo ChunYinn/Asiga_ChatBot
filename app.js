7d8d2715c11c70e982596f43f0965fe13105a330
Chun-Yin Li <chli5570@uni.sydney.edu.au>
2023-08-21 01:56:47 +1000

    Chunyin add delete and new chat function

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const userInput = document.querySelector('.user-input');
 const chatContainer = document.querySelector('.chat-container');
 let isFirstMessage = true;  
 
-let currentDate = new Date();
-let formattedDate = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')} ${currentDate.getHours().toString().padStart(2, '0')}:${currentDate.getMinutes().toString().padStart(2, '0')}`;
-
 generateMsg = () => {
     const userMessage = userInput.value;
 
-    if (userMessage.trim() === "") return;  // Check if message is empty
+    //empty msg
+    if (userMessage.trim() === "") return; 
+
+    //date generator
+    let currentDate = new Date();
+    let formattedDate = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')} ${currentDate.getHours().toString().padStart(2, '0')}:${currentDate.getMinutes().toString().padStart(2, '0')}`;
 
     // Append user message to main chat
     const userMessageDiv = document.createElement('div');
@@ -41,22 +43,25 @@ generateMsg = () => {
     userInput.value = "";
     chatContainer.scrollTop = chatContainer.scrollHeight;
 
-    //Append to chat history
+    // Append to chat history
     if (isFirstMessage) {
-      const historyContainer = document.querySelector('.history');
-      const chatHistoryItem = document.createElement('div');
-      chatHistoryItem.className = 'history-item';
-      chatHistoryItem.innerHTML = `<p>${formattedDate}</p>`;
-  
-      // Check if there's any child in the historyContainer
-      if (historyContainer.firstChild) {
-          historyContainer.insertBefore(chatHistoryItem, historyContainer.firstChild);
-      } else {
-          historyContainer.appendChild(chatHistoryItem);
-      }
-  
-      isFirstMessage = false; 
-  }
+        const historyContainer = document.querySelector('.history');
+        const chatHistoryItem = document.createElement('div');
+        chatHistoryItem.className = 'history-item';
+        chatHistoryItem.innerHTML = `
+            <p>${formattedDate}</p>
+            <div class="delete-icon">&#10006;</div>
+        `;
+
+        // Check if there's any child in the historyContainer
+        if (historyContainer.firstChild) {
+            historyContainer.insertBefore(chatHistoryItem, historyContainer.firstChild);
+        } else {
+            historyContainer.appendChild(chatHistoryItem);
+        }
+
+        isFirstMessage = false; 
+    }
 }
 
 submitBtn.addEventListener('click', generateMsg);
@@ -64,7 +69,6 @@ submitBtn.addEventListener('click', generateMsg);
 
 
 //---------------------user-input text area---------------------------------------
-
 
 //text area adjustions
 userInput.addEventListener('keydown', function(event) {
@@ -92,7 +96,7 @@ userInput.addEventListener('keydown', function(e) {
   }
 });
 
-//------------Collapse btn----
+//------------Collapse btn---------------------------
 const sidebar = document.querySelector('.side-bar');
 const mainContent = document.querySelector('.main');
 const collapseBtn = document.getElementById('collapseBtn');
@@ -125,3 +129,41 @@ openBtn.addEventListener('click', function() {
 
 // still figuring
 
+
+// delete icon for history chat----------
+document.addEventListener('click', function(e) {
+    if (e.target && e.target.classList.contains('delete-icon')) {
+        let historyItem = e.target.closest('.history-item');
+        if (historyItem) {
+            historyItem.remove();
+        }
+    }
+});
+
+// New Chat btn -----------------------------------
+const newChatBtn = document.querySelector('.new-chat-btn');
+
+newChatBtn.addEventListener('click', function() {
+    // Clear chat messages
+    chatContainer.innerHTML = '';
+
+    // Reset isFirstMessage flag
+    isFirstMessage = true;
+
+    // Add the initial chatbot message again.
+    const chatbotMessageWrapper = document.createElement('div');
+    chatbotMessageWrapper.className = 'chatbot-message-wrapper';
+
+    const robotIcon = document.createElement('img');
+    robotIcon.src = "resource/robot.png";
+    robotIcon.alt = "Robot Icon";
+    robotIcon.className = "robot-icon";
+    chatbotMessageWrapper.appendChild(robotIcon);
+
+    const chatbotMessageDiv = document.createElement('div');
+    chatbotMessageDiv.className = 'chatbot-message message';
+    chatbotMessageDiv.innerHTML = `<p>Asiga GPT: How can I help you?</p>`;
+    chatbotMessageWrapper.appendChild(chatbotMessageDiv);
+
+    chatContainer.appendChild(chatbotMessageWrapper);
+});
