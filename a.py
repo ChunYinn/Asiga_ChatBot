7ce82543a6816b3771309bd97e7d40ee8c3add17
Chun-Yin Li <chli5570@uni.sydney.edu.au>
2023-10-01 23:54:28 +1100

    langchain with history chat be able to continue asking

--- a.py
+++ a.py
@@ -31,63 +31,77 @@ def load_all_documents_in_directory(directory_path):
   print(f"Loaded {success_count} files, failed to load {failed_count} files.")
   return documents
 
-def trained_gpt(question):
-  os.environ['OPENAI_API_KEY'] = config.API_KEY
+# def trained_gpt(question):
+#   os.environ['OPENAI_API_KEY'] = config.API_KEY
  
-  directory_path = "src/real_data/quick-answers"
-  documents = load_all_documents_in_directory(directory_path)
+#   directory_path = "src/real_data/quick-answers"
+#   documents = load_all_documents_in_directory(directory_path)
 
-  text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
-  texts = text_splitter.split_documents(documents)
+#   text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
+#   texts = text_splitter.split_documents(documents)
 
-  embeddings = OpenAIEmbeddings()
-  docsearch = Chroma.from_documents(texts, embeddings)
+#   embeddings = OpenAIEmbeddings()
+#   docsearch = Chroma.from_documents(texts, embeddings)
   
-  custom_template = """You are the customer support assistant. If you don't know the answer, just say that you don't know respectfully, don't try to make up an answer, and if you can't understand the question, just say you don't understand the question, tell the customer to ask again. Please provide the answer in English and the answer should be detailed:
-  {context}
+#   custom_template = """You are the customer support assistant.  If you don't know the answer, just say that you don't know respectfully, don't try to make up an answer, and if you can't understand the question, just say you don't understand the question, tell the customer to ask again. Please provide the answer in English and the answer should be detailed:
+#   {context}
   
-  Question: {question}
-  Answer in English Respectfully:"""
+#   Question: {question}
+#   Answer in English Respectfully:"""
   
-  PROMPT = PromptTemplate(
-      template=custom_template, input_variables=["context", "question"]
-  )
+#   PROMPT = PromptTemplate(
+#       template=custom_template, input_variables=["context", "question"]
+#   )
   
-  embeddings = OpenAIEmbeddings()
-  chain_type_kwargs = {"prompt": PROMPT}
-  qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever(search_type="mmr", search_kwargs={'k': 2}), chain_type_kwargs=chain_type_kwargs)
+#   embeddings = OpenAIEmbeddings()
+#   chain_type_kwargs = {"prompt": PROMPT}
+#   qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever(search_type="mmr", search_kwargs={'k': 5, 'fetch_k': 50}), chain_type_kwargs=chain_type_kwargs)
 
-  return qa.run(question)
+#   return qa.run(question)
 
 
 # from langchain.indexes import VectorstoreIndexCreator
 # index = VectorstoreIndexCreator().from_loaders([loader, loader2])
 
-# query = "What is the composer command line option to clear composer settings?"
+query = "What is the composer command line option to clear composer settings?"
 # print(trained_gpt(query))
 
 #------------------------------------------------------------
-# from langchain.embeddings.openai import OpenAIEmbeddings
-# from langchain.vectorstores import Chroma
-# from langchain.text_splitter import CharacterTextSplitter
-# from langchain.llms import OpenAI
-# from langchain.chains import ConversationalRetrievalChain
-# from langchain.memory import ConversationBufferMemory
-# from langchain.chat_models import ChatOpenAI
-# from langchain.prompts.prompt import PromptTemplate
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.prompt import PromptTemplate
 
-# # def trained_gpt_with_history(question):
-# chat_history = []
-# os.environ['OPENAI_API_KEY'] = config.API_KEY
+# def trained_gpt_with_history(question):
+chat_history = []
 
-# directory_path = "src/real_data/quick-answers"
-# documents = load_all_documents_in_directory(directory_path)
+custom_template = """
+You are an assistant. Given the chat history and a new question, provide a detailed and comprehensive answer. If you don't know the answer, kindly state that you're unsure. 
 
-# text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
-# documents = text_splitter.split_documents(documents)
+Chat History:
+{chat_history}
 
-# embeddings = OpenAIEmbeddings()
-# vectorstore = Chroma.from_documents(documents, embeddings)
+Question: {question}
+Answer respectfully:"""
+
+CUSTOM_QUESTION_PROMPT = PromptTemplate.from_template(custom_template)
+
+
+
+os.environ['OPENAI_API_KEY'] = config.API_KEY
+
+directory_path = "src/real_data/quick-answers"
+documents = load_all_documents_in_directory(directory_path)
+
+text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
+documents = text_splitter.split_documents(documents)
+
+embeddings = OpenAIEmbeddings()
+vectorstore = Chroma.from_documents(documents, embeddings)
 
 # def get_chat_history(inputs) -> str:
 #     res = []
@@ -95,16 +109,20 @@ def trained_gpt(question):
 #         res.append(f"Human:{human}\nAI:{ai}")
 #     return "\n".join(res)
   
-# qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0), vectorstore.as_retriever(), get_chat_history=get_chat_history)
+model = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.3)
+qa = ConversationalRetrievalChain.from_llm(
+    model,
+    vectorstore.as_retriever(search_type="similarity_score_threshold", search_kwargs={'score_threshold': 0.8}),
+    condense_question_prompt=CUSTOM_QUESTION_PROMPT
+)
 
-# chat_history = []
-# result = qa({"question": query, "chat_history": chat_history})
-# print(result['answer'])
+query = "what if the Trays imperfect?"
+result = qa({"question": query, "chat_history": chat_history})
+print(result['answer'])
 
-# query = "what is the command you provide mean for?"
-# result = qa({"question": query, "chat_history": chat_history})
-# print(result['answer'])
+chat_history.append((query, result["answer"]))
 
-# query = "--profile-reset menas elp you get the debug log, error log, and printer.ini files from your printer?"
-# result = qa({"question": query, "chat_history": chat_history})
-# print(result['answer'])
+query = "what is the command you provide mean for?"
+result = qa({"question": query, "chat_history": chat_history})
+print(result['answer'])
+
---
+++ finetune.py
@@ -0,0 +1,14 @@
+import os
+import openai
+openai.api_key = "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"
+openai.File.create(
+  file=open("train_data.jsonl", "rb"),
+  purpose='fine-tune'
+)
+
+
+openai.FineTuningJob.create(training_file="file-abc123", model="gpt-3.5-turbo")
+
+# Retrieve the state of a fine-tune
+openai.FineTuningJob.retrieve("ft-abc123")
+
